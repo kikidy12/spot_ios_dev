ae402c7d35a37976fb3c61d844d2fb29dcb13105
kikidy12 <30651543+kikidy12@users.noreply.github.com>
2019-08-01 10:05:39 +0900

    Initial Commit

---
+++ fifteensecond/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fifteensecond
+//
+//  Created by 권성민 on 01/08/2019.
+//  Copyright © 2019 cfour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
